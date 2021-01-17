073b7b59d3d8129af694603ca5c2d24ad47a10f7
Manuel Henriquez <manuelhm1993@gmail.com>
2021-01-17 03:04:24 -0400

    Compilado el archivo utilidades
    Se realizó la importación del archivo utilidades requiriendolo desde resources con la sintaxis de node.js, es realmente sencillo

--- public/js/app.js
+++ public/js/app.js
@@ -49797,7 +49797,9 @@ Vue.component('example-component', __webpack_require__(/*! ./components/ExampleC
 
 var app = new Vue({
   el: '#app'
-});
+}); //Archivo propio
+
+__webpack_require__(/*! ./utilidades */ "./resources/js/utilidades.js");
 
 /***/ }),
 
@@ -49912,6 +49914,36 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_ExampleComponent_vue_vue_type_template_id_299e239e___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
 
 
+
+/***/ }),
+
+/***/ "./resources/js/utilidades.js":
+/*!************************************!*\
+  !*** ./resources/js/utilidades.js ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+//Utilidades y comportamientos propios de la aplicación
+var urlUsers = 'https://jsonplaceholder.typicode.com/users';
+new Vue({
+  el: '#main',
+  created: function created() {
+    this.devolverUsuarios();
+  },
+  data: {
+    usuarios: []
+  },
+  methods: {
+    devolverUsuarios: function devolverUsuarios() {
+      var _this = this;
+
+      axios.get(urlUsers).then(function (response) {
+        _this.usuarios = response.data;
+      });
+    }
+  }
+});
 
 /***/ }),
 

d5154911404d257b11b582b2971f716a54daf871
Manuel Henriquez <manuelhm1993@gmail.com>
2021-01-17 19:43:05 -0400

    Vista dashboard terminada
    Se realizó el maquetado para el panel administrativo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>Desde dashboard</p>
+    <div class="row">
+        <div class="col-12">
+            <h1 class="pb-2 mt-4 mb-2 border-bottom">CRUD Laravel y Vue.js</h1>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-7">
+            <div class="row">
+                <div class="col-12">
+                    <a href="#" class="btn btn-primary float-right">Nueva tarea</a>
+                </div>
+            </div>
+
+            <div class="row mt-4">
+                <div class="col-12">
+                    <table class="table table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Tarea</th>
+                                <th scope="col" colspan="2">&nbsp;</th>
+                            </tr>
+                        </thead>
+        
+                        <tbody>
+                            <tr>
+                                <td width="10px">1</td>
+                                <td>Tarea 1</td>
+                                <td width="10px">
+                                    <a href="#" class="btn btn-warning btn-sm">Editar</a>
+                                </td>
+                                <td width="10px">
+                                    <a href="#" class="btn btn-danger btn-sm">Eliminar</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-5">
+            <pre class="bg-ligth border">
+                <code>
+                    @{{ $data }}
+                </code>
+            </pre>
+        </div>
+    </div>
 @endsection

7690246214e2582ef1936f5c497ee309a288ebb8
Manuel Henriquez <manuelhm1993@gmail.com>
2021-01-18 01:14:40 -0400

    Forma de guardar con formularios
    Se puede realizar el envío de datos usando formularios, pero como se usa axios, no es necesario

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,3 +1,4 @@
+{{-- Se puede hacer la actividad sin formularios --}}
 <div class="modal fade" id="create" tabindex="-1" aria-labelledby="createLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -18,3 +19,29 @@
         </div>
     </div>
 </div>
+
+{{-- Forma de hacerlo usando formularios --}}
+{{-- 
+<form action="POST" v-on:submit.prevent="storeKeep">
+    <div class="modal fade" id="create" tabindex="-1" aria-labelledby="createLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="createLabel">Nueva tarea</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <label for="keep">Rótulo</label>
+                    <input type="text" name="keep" class="form-control" v-model="newKeep">
+                    <span v-for="error in errors" v-text="error" class="text-danger"></span>
+                </div>
+                <div class="modal-footer">
+                    <input type="submit" class="btn btn-primary" value="Crear">
+                </div>
+            </div>
+        </div>
+    </div>
+</form>    
+--}}
